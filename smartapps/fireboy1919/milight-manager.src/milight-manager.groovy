d250c6b77a3d9c3dc8e676b9d1c8f29c329a6aa8
Rusty Phillips <rusty@cloudspace.com>
2017-04-10 15:07:05 -0400

    Fixed another typo bug.

--- smartapps/fireboy1919/milight-manager.src/milight-manager.groovy
+++ smartapps/fireboy1919/milight-manager.src/milight-manager.groovy
@@ -55,10 +55,10 @@ def httpCall(body, uri, mac, evt) {
         body: body, 
         contentType: 'application/json'
     ]
-    log.debug "Sending to ${params.uri}${path}."
+    log.debug("Sending to ${params['uri']}${path}.")
     try {
         httpPutJson(params) {resp ->
-            if(settings.isDebug) { log.debug "MiLight device: ${mac}, paramsfrom cloud: ${params}" }
+            if(settings.isDebug) { log.debug "MiLight device: ${mac}" }
             //parseResponse(resp, mac, evt)
         }
     } catch (e) {
