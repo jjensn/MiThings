d9c47f2187a5ef9a2db4e93dde92482d64b8b9cd
Rusty Phillips <rusty@cloudspace.com>
2017-04-10 16:16:05 -0400

    Fixed error.

--- smartapps/fireboy1919/milight-manager.src/milight-manager.groovy
+++ smartapps/fireboy1919/milight-manager.src/milight-manager.groovy
@@ -51,14 +51,14 @@ def initialize() {
 def httpCall(body, uri, mac, evt) {
 	def group =  evt.device.getPreferences()["group"]
     def path =  "/gateways/$mac/rgbw/$group"
-    def body = JsonOutput.toJson(body)
+    def bodyString = JsonOutput.toJson(body)
     def params = [
         uri:  uri,
         path: path,
         body: body, 
         contentType: 'application/json'
     ]
-    log.debug("Sending to ${params['uri']}${path}.  Body: ${body}.")
+    log.debug("Sending to ${params['uri']}${path}.  Body: ${bodyString}.")
     try {
         httpPutJson(params) {resp ->
             if(settings.isDebug) { log.debug "Successfully updated settings." }
